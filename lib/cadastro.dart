3d2186ef0762ecf2f666107a6f93f407f1a5a703
Natalia Bento <nataliabento97@gmail.com>
2024-11-01 12:07:59 -0300

    feat: adiciona mais campos no cadastro e inclui um campo de numero de

--- lib/cadastro.dart
+++ lib/cadastro.dart
@@ -9,10 +9,17 @@ class TelaDeCadastro extends StatefulWidget {
 
 class _TelaDeCadastroState extends State<TelaDeCadastro> {
   bool _isPasswordVisible = false;
+  bool _isConfirmPasswordVisible = false;
 
   void _togglePasswordVisibility() {
     setState(() {
       _isPasswordVisible = !_isPasswordVisible;
+    });
+  }
+
+  void _toggleConfirmPasswordVisibility() {
+    setState(() {
+      _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
     });
   }
 
@@ -43,7 +50,7 @@ class _TelaDeCadastroState extends State<TelaDeCadastro> {
             ),
             Image.asset(
               'assets/images/foto_de_menu_suspenso.png',
-              height: 40, // Ajuste a altura conforme necessário
+              height: 40,
             ),
             Row(
               mainAxisSize: MainAxisSize.min,
@@ -69,6 +76,7 @@ class _TelaDeCadastroState extends State<TelaDeCadastro> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(height: 24),
+
             // Campo de Nome
             const Text(
               'Nome',
@@ -105,9 +113,9 @@ class _TelaDeCadastroState extends State<TelaDeCadastro> {
             ),
             const SizedBox(height: 16),
 
-            // Campo de Senha
+            // Campo de Criar Senha
             const Text(
-              'Senha',
+              'Crie uma senha',
               style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8),
@@ -129,6 +137,70 @@ class _TelaDeCadastroState extends State<TelaDeCadastro> {
                 ),
               ),
             ),
+            const SizedBox(height: 16),
+
+            // Campo de Confirmar Senha
+            const Text(
+              'Confirme sua senha',
+              style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            TextField(
+              obscureText: !_isConfirmPasswordVisible,
+              decoration: InputDecoration(
+                hintText: 'Confirme sua senha',
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _isConfirmPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                    color: Colors.grey,
+                  ),
+                  onPressed: _toggleConfirmPasswordVisibility,
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            // Campo de Idade
+            const Text(
+              'Idade',
+              style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                hintText: 'Insira sua idade',
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            // Campo de Número do Plano
+            const Text(
+              'Número da Carteirinha (Opcional)',
+              style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Insira seu número da carteirinha (opcional)',
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+            ),
+            const SizedBox(height: 4),
             const SizedBox(height: 32),
 
             // Botão de "Cadastrar"
